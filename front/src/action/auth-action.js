718565cba4a529e4c5123014c525888b79988ad7
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-06 16:26:38 -0700

    Added actions for API calls

--- front/src/action/auth-action.js
+++ front/src/action/auth-action.js
@@ -3,9 +3,34 @@ import superagent from 'superagent';
 
 export const setToken = (token) => ({
   type: 'TOKEN_SET',
-  payload: 'token'
+  payload: token
 });
 
 export const deleteToken = () => ({
   type: 'TOKEN_DELETE'
 })
+
+export const signupRequest => user => dispatch => {
+  return superagent.post(`${__API_URL__}/signup`)
+  .withCredentials()
+  .send(user)
+  .then(res => {
+    dispatch(setToken(res.text));
+    try {
+      localStorage.token = res.text;
+    } catch (err) {
+      console.log(err);
+    }
+    return res;
+  })
+}
+
+export const loginRequest = user => dispatch => {
+  return superagent.get(`${__API_URL__}/login`)
+  .auth(uers.username, user.password)
+  .withCredentials()
+  .then(res => {
+    dispatch(setToken(res.text));
+    return res;
+  })
+}

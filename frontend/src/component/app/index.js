1039aea51d54e3278c3576fe66d7a5da4475c688
ohjonah <ohjonah@gmail.com>
2017-09-06 16:23:11 -0700

    Creates Auth Actions

--- frontend/src/component/app/index.js
+++ frontend/src/component/app/index.js
@@ -1,1 +1,14 @@
-console.log('HI THERE')
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        HI
+      </div>
+    )
+  }
+}
+
+export default App;

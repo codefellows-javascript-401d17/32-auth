c4a4c34d84784690160981084577d5c65009d764
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-10 20:17:14 -0700

    Winner winner chicken dinner

--- front/src/lib/valiation.js
+++ front/src/lib/valiation.js
@@ -1,10 +1,12 @@
 module.exports = (profile, preReqs) => {
   let incomplete = false;
+
   for (let i = 0; i < preReqs.length; i++) {
     if(!profile[preReqs[i]]) {
       console.error(`VALIDATION ERROR: Missing ${preReqs[i]} property.`);
       incomplete = true;
     }
   }
+
   if(incomplete) throw new Error('VALIDATION ERROR: process terminated.');
 };

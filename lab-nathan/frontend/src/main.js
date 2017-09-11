7b577c2a322753cbc8580f67ae0d85c1319bd355
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-09-10 23:43:25 -0700

    Add profile creation with image uplaods.

--- lab-nathan/frontend/src/main.js
+++ lab-nathan/frontend/src/main.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './components/app/app.js';
+import createStore from './lib/create-store.js';
 
-ReactDom.render(<App />, document.getElementById('root'));
+let AppContainer = () => {
+  return (
+    <Provider store={createStore()}>
+      <App />
+    </Provider>
+  )
+}
+
+ReactDom.render(<AppContainer />, document.getElementById('root'));
